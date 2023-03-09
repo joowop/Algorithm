f9ca1fe7f4e56f51d00bda9dbb5f007d982eb3a8
seo ju wan <80230688+joowop@users.noreply.github.com>
2023-03-09 15:55:07 +0900

    [level 1] Title: 행렬의 덧셈, Time: 32.72 ms, Memory: 22.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -0,0 +1,10 @@
+def solution(arr1, arr2):
+    answer =[]
+    for i in range(len(arr1)):
+        sublist = []
+        for j in range(len(arr1[i])):
+            sublist.append(arr1[i][j] + arr2[i][j])
+        answer.append(sublist)
+
+    
+    return answer
