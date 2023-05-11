b6b5ec27efe639137368ef268d43c2a0be4b84f8
seo ju wan <80230688+joowop@users.noreply.github.com>
2023-05-11 21:26:17 +0900

    [level 0] Title: 대문자와 소문자, Time: 0.30 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120893. 대문자와 소문자/대문자와 소문자.py
@@ -0,0 +1,11 @@
+def solution(my_string):
+    return my_string.swapcase()
+def solution(my_string):
+    answer = ''
+
+    for i in my_string:
+        if i.isupper():
+            answer+=i.lower()
+        else:
+            answer+=i.upper()
+    return answer
